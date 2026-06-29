958f12f8efc979ba948bf2274c9a46a3f687a25f
SimKDT <simon.cadet79@hotmail.fr>
2026-06-29 22:30:20 +0200

    current

--- chores/docs/xml.py
+++ chores/docs/xml.py
@@ -10,6 +10,7 @@ from utility.rst import headers
 
 
 
+## DEFINITIONS
 
 TOC_DESCRIPTION = """
 Reference documentation for XML file formats used in Project Zomboid. With this documentation, comes settings that you can use for `VSCode <https://pzwiki.net/wiki/Visual_Studio_Code>`_ alongside the `RedHat XML <https://marketplace.visualstudio.com/items?itemName=redhat.vscode-xml>`_ extension to automatically verify your files. You can find `here <https://github.com/PZ-Wiki-Modding/pz-xml-data/blob/main/out/settings.json>`_ these settings.
